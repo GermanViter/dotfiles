fc97d39a48b5985b7d3fb9c9c5e5809e07ba4fd5
GermanViter <german.viter@icloud.com>
2026-03-21 01:43:31 -0400

    fixed warnings

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -6,42 +6,42 @@ vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.wo.relativenumber = true
-vim.opt.shortmess:append("atIc")
-vim.opt.cmdheight = 2
-vim.opt.report = 9999
 
+-- Suppress startup intro and common messages
+vim.opt.shortmess:append("I")
 
-
--- Suppress warnings, only show errors
-vim.notify = function(msg, log_level, _)
-	if log_level == vim.log.levels.ERROR then
-		vim.api.nvim_echo({ { tostring(msg), "ErrorMsg" } }, true, {})
-	end
+-- Filter out all warnings from notifications (only show errors)
+local original_notify = vim.notify
+vim.notify = function(msg, level, opts)
+  if level == vim.log.levels.WARN or level == "warn" then
+    return
+  end
+  original_notify(msg, level, opts)
 end
 
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-	if vim.v.shell_error ~= 0 then
-		vim.api.nvim_echo({
-			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-			{ out, "WarningMsg" },
-			{ "\nPress any key to exit..." },
-		}, true, {})
-		vim.fn.getchar()
-		os.exit(1)
-	end
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- Setup lazy.nvim
 require("lazy").setup({
-	spec = {
-		-- Import all plugins from lua/plugins/ directory
-		{ import = "plugins" },
-	},
-	install = { colorscheme = { "catppuccin-mocha" } },
-	checker = { enabled = true },
+  spec = {
+    -- Import all plugins from lua/plugins/ directory
+    { import = "plugins" },
+  },
+  install = { colorscheme = { "catppuccin-mocha" } },
+  checker = { enabled = true },
 })
--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -1,93 +1,89 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    lazy = false,
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    lazy = false,
-    opts = {
-      auto_install = true,
-    },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-    },
-    lazy = false,
-    config = function()
-      local capabilities = {}
-      local has_cmp, cmp_lsp = pcall(require, "cmp_nvim_lsp")
-      if has_cmp then
-        capabilities = cmp_lsp.default_capabilities()
-      end
+	{
+		"williamboman/mason.nvim",
+		lazy = false,
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		lazy = false,
+		opts = {
+			auto_install = true,
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+		},
+		lazy = false,
+		config = function()
+			local capabilities = {}
+			local has_cmp, cmp_lsp = pcall(require, "cmp_nvim_lsp")
+			if has_cmp then
+				capabilities = cmp_lsp.default_capabilities()
+			end
 
-      -- Configure diagnostics
-      vim.diagnostic.config({
-        virtual_text = true,
-        signs = true,
-        underline = true,
-        update_in_insert = true,
-        severity_sort = true,
-      })
+			-- Change diagnostic signs to icons
+			local signs = { Error = " ", Warn = " ", Hint = "󰌵", Info = " " }
+			for type, icon in pairs(signs) do
+				local hl = "DiagnosticSign" .. type
+				vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+			end
 
-      -- Change diagnostic signs to icons
-      local signs = { Error = " ", Warn = " ", Hint = "󰌵", Info = " " }
-      for type, icon in pairs(signs) do
-        local hl = "DiagnosticSign" .. type
-        vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-      end
+			local lspconfig = require("lspconfig")
 
-      local lspconfig = require("lspconfig")
+			local on_attach = function(_, bufnr)
+				local opts = { buffer = bufnr }
+				vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+				vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, opts)
+				vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, opts)
+				vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
 
-      local on_attach = function(_, bufnr)
-        local opts = { buffer = bufnr }
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-        vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, opts)
-        vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, opts)
-        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+				-- Diagnostic keymaps
+				vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
+				vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
+				vim.keymap.set(
+					"n",
+					"<leader>e",
+					vim.diagnostic.open_float,
+					{ desc = "Open floating diagnostic message" }
+				)
+				vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
+			end
 
-        -- Diagnostic keymaps
-        vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
-        vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
-        vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
-        vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
-      end
+			-- TypeScript/JavaScript
+			lspconfig.ts_ls.setup({
+				capabilities = capabilities,
+				on_attach = on_attach,
+			})
 
-      -- TypeScript/JavaScript
-      lspconfig.ts_ls.setup({
-        capabilities = capabilities,
-        on_attach = on_attach,
-      })
+			-- Ruby
+			lspconfig.solargraph.setup({
+				capabilities = capabilities,
+				on_attach = on_attach,
+			})
 
-      -- Ruby
-      lspconfig.solargraph.setup({
-        capabilities = capabilities,
-        on_attach = on_attach,
-      })
+			-- HTML
+			lspconfig.html.setup({
+				capabilities = capabilities,
+				on_attach = on_attach,
+			})
 
-      -- HTML
-      lspconfig.html.setup({
-        capabilities = capabilities,
-        on_attach = on_attach,
-      })
-
-      -- Lua
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities,
-        on_attach = on_attach,
-        settings = {
-          Lua = {
-            diagnostics = {
-              globals = { "vim" },
-            },
-          },
-        },
-      })
-    end,
-  },
+			-- Lua
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+				on_attach = on_attach,
+				settings = {
+					Lua = {
+						diagnostics = {
+							globals = { "vim" },
+						},
+					},
+				},
+			})
+		end,
+	},
 }
