7183af14eff7331a3fadfb00dc33e56ecad356c6
GermanViter <german.viter@icloud.com>
2026-04-06 23:28:56 -0400

    style(nvim plugins) : try to disable the tab bar that I dont use

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -6,3 +6,4 @@ vim.opt.clipboard = "unnamedplus" -- Use the system clipboard for all operations
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
+vim.opt.showtabline = 0 -- Disable the tabline
