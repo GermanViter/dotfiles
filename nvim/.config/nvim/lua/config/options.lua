d9e24bc9658f16fd79832e2fceb05a4b64e50f1b
GermanViter <228225241+GermanViter@users.noreply.github.com>
2026-05-09 16:42:20 -0400

    start a new nvim config

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,7 +1,3 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
-
 vim.opt.clipboard = "unnamedplus" -- Use the system clipboard for all operations
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
